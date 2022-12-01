b99b683d5b0c9ae03b365f00082f17768049e56b
lbaum <lbaumgaertner@culba.de>
2022-12-01 15:10:48 +0100

    hide action bar and change status bar color to white

--- app/src/main/java/com/example/challenge/MainActivity.kt
+++ app/src/main/java/com/example/challenge/MainActivity.kt
@@ -38,10 +38,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        supportActionBar?.hide()
+//        supportActionBar?.hide()
 
 
         val baseFloatingActionButton = findViewById<View>(R.id.baseFloatingActionButton)
