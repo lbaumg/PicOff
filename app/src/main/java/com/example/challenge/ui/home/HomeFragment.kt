3fe0dcc0995b922b701832d711382d8ccd82eac5
lbaum <lbaumgaertner@culba.de>
2022-12-01 18:51:12 +0100

    sent/received buttons improved

--- app/src/main/java/com/example/challenge/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/challenge/ui/home/HomeFragment.kt
@@ -1,18 +1,19 @@
 package com.example.challenge.ui.home
 
-import android.R
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.challenge.R
 import com.example.challenge.databinding.FragmentHomeBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
+    private var onReceiveFragment = true
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,13 +29,35 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = binding.root
 
-        return root
+        val buttonSent =  view.findViewById<Button>(R.id.buttonSent)
+        val buttonReceived = view.findViewById<Button>(R.id.buttonReceived)
+
+        buttonReceived.paintFlags = buttonReceived.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        buttonSent.setOnClickListener {
+            if (onReceiveFragment) {
+                buttonSent.paintFlags = buttonSent.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+                buttonReceived.paintFlags = buttonReceived.paintFlags and Paint.UNDERLINE_TEXT_FLAG.inv()
+                onReceiveFragment = false
+            }
+        }
+        buttonReceived.setOnClickListener {
+            if (!onReceiveFragment) {
+                buttonReceived.paintFlags = buttonReceived.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+                buttonSent.paintFlags = buttonSent.paintFlags and Paint.UNDERLINE_TEXT_FLAG.inv()
+                onReceiveFragment = true
+            }
+        }
+
+        return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
